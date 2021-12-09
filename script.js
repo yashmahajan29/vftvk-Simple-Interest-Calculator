790813ae5a2c5d591af7e9f852c636a1e05c268b
yashmahajan29 <yashsmahajan82@gmail.com>
2021-12-10 03:57:11 +0530

    minor changes

--- script.js
+++ script.js
@@ -3,11 +3,7 @@ function compute() {
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest= principal * years * rate / 100;
-    var year = new Date().getFullYear(); 
-    var final= parseInt(year,10) + parseInt(years,10);
-    var result= "If you deposit <mark>" + principal + "</mark>,<br> at an interest rate of <mark>" + rate + "</mark> % <br> You will receive an amount of <mark>" + interest + ",</mark><br> in the year <mark>" + final + "</mark>";
-    document.getElementById("result").innerHTML=result;
-
+    
     if (principal == "") { 
         alert("Enter a positive number");
         document.getElementById("principal").focus();
@@ -20,6 +16,11 @@ function compute() {
             return false;
         }
     }
+
+    var year = new Date().getFullYear(); 
+    var final= parseInt(year,10) + parseInt(years,10);
+    var result= "If you deposit <mark>" + principal + "</mark>,<br> at an interest rate of <mark>" + rate + "</mark> % <br> You will receive an amount of <mark>" + interest + ",</mark><br> in the year <mark>" + final + "</mark>";
+    document.getElementById("result").innerHTML=result;
 }
 function updateRate(newVal) 
 {
